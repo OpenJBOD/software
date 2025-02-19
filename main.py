fe7f1da75b92f5fd4b6bfbf5d14fb5368e3e4ea9
TheGuyDanish <5776313+TheGuyDanish@users.noreply.github.com>
2025-02-19 17:54:21 +0100

    Update stylesheet links and fix template variable display issues; bump version to 1.3.0-DEV

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ uart0 = UART(0)
 uart0.init(tx=16, rx=17)
 os.dupterm(uart0)
 
-VERSION = "1.2.0"
+VERSION = "1.3.0-DEV"
 DEFAULT_CONFIG = {
     "network": {
         "hostname": "openjbod",
@@ -123,8 +123,6 @@ def power_debounce(pin):
     power_btn.irq(handler=None)
     pwr_timer.init(mode=Timer.ONE_SHOT, period=200, callback=power_btn_handler)
 
-
-# CHECK THESE FOR REV4
 # Busses
 i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=100000)
 spi = SPI(0, 2000000, mosi=Pin(3), miso=Pin(4), sck=Pin(2))
